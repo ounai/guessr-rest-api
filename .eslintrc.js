fc82b85e3804666f66cb6e74e47588a69064eb01
Onni Rantanen <ounaifi@gmail.com>
2022-04-20 07:36:11 +0000

    TypeScript configuration

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prefer-arrow'
+  ],
+  rules: {
+    'max-len': ['warn', { code: 120 }],
+    'max-lines-per-function': ['warn', 50],
+    semi: ['error', 'always'],
+    'eol-last': ['error', 'always'],
+    'no-console': ['warn'],
+    'node/no-callback-literal': 'off',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 0,
+        maxBOF: 0
+      }
+    ],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false
+      }
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      { allowNamedFunctions: true }
+    ],
+    'func-style': [
+      'error',
+      'expression',
+      { allowArrowFunctions: true }
+    ]
+  }
+};
